9782af0fb12a0684e325033460c27f4589935ca2
marc pare <smac.1@hotmail.com>
2023-09-20 19:08:44 -0400

    Salut Martin

---
+++ assets/js/functions.js
@@ -0,0 +1,22 @@
+$( document ).ready(function() {
+	
+function nav(){
+	
+	$('.nav-toggle').click(function(){
+		
+		$('.nav').toggleClass('open');
+		
+	});
+	
+}
+
+nav();
+
+});
+
+var map = L.map('map').setView([45.4011, -71.88], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker([45.40820, -71.88290]).addTo(map);
